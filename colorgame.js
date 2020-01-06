31c9300e7c6637c85d654c2fa020b2ecf5893196
Saurav <47532084+Saurav@users.noreply.github.com>
2020-01-06 23:08:29 +0530

    Added Reset Button

--- colorgame.js
+++ colorgame.js
@@ -1,15 +1,24 @@
-var colors= [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(255, 0, 255)",
-	"rgb(0, 0, 255)",
-	];
+var colors= generateColors(6);
 var squares=document.querySelectorAll(".square");
 var colorDisplay=document.querySelector("#colorDisplay");
 var pickedColor=pickColor();
 var messageDisplay=document.querySelector("#message");
+var h1=document.querySelector("h1");
+var resetButton=document.querySelector("#reset");
+
+
+	resetButton.addEventListener("click",function(){
+		colors=generateColors(6);
+		pickedColor=pickColor();
+		colorDisplay.textContent=pickedColor;
+
+		for(var i=0;i<squares.length;i++){
+		//adding colors
+		squares[i].style.backgroundColor = colors[i];
+		}
+		h1.style.backgroundColor="#232323";
+		
+	});
 
 
 	for(var i=0;i<squares.length;i++){
@@ -21,8 +30,11 @@ var messageDisplay=document.querySelector("#message");
 			var clickedColor=this.style.backgroundColor;;
 
 			if(clickedColor===pickedColor){
+				console.log(clickedColor+"==="+pickedColor);	
 				messageDisplay.textContent="Correct";
 				changeColor(clickedColor);
+				h1.style.backgroundColor=clickedColor;
+				resetButton.textContent="Play again?";
 			}
 				
 			else{
@@ -44,4 +56,21 @@ var messageDisplay=document.querySelector("#message");
 	function pickColor(){
 		var random=Math.floor(Math.random() *colors.length);
 		return colors[random];
+	}
+	function generateColors(num){
+		var arr	= [];
+
+		for(var i=0;i<num;i++){
+			arr.push(randomColor());
+		}
+
+
+		return arr;
+	}
+
+	function randomColor(){
+		var red=Math.floor(Math.random() * 256);
+		var blue=Math.floor(Math.random() * 256);
+		var green=Math.floor(Math.random() * 256);
+		return "rgb("+red+", "+green+", "+blue+")";
 	}
